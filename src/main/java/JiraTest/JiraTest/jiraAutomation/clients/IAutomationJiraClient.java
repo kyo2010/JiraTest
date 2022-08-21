3370802a14810cd768e2f6b0bf82461dea9e6740
Konstantin Kimlaev <konstantin.kimlaev@ru.panasonic.com>
2022-08-21 11:10:02 +0300

    change folders

--- src/main/java/JiraTest/JiraTest/jiraAutomation/clients/IAutomationJiraClient.java
+++ src/main/java/JiraTest/JiraTest/jiraAutomation/clients/IAutomationJiraClient.java
@@ -6,8 +6,10 @@ import java.util.Map;
 
 public interface IAutomationJiraClient {
 
+    /** Receive all project IssueTypes, name->id */
     Map<String, Long> getTaskIdByName(String projectKey) throws AutomationException;
 
+    /** Create Issue */
     String createIssue(String projectKey, Long issueType, String summary, Long priorityID) throws AutomationException;
 
 }
